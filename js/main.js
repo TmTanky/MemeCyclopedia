d956f5050fd7400c983f4d8faa4266ff9cc0f077
TmLololol <tmtanky@gmail.com>
2021-03-17 23:56:05 +0800

    1st commit done

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const meme1 = document.querySelector(`.meme1`)
+const meme2 = document.querySelector(`.meme2`)
+const meme3 = document.querySelector(`.meme3`)
+const meme4 = document.querySelector(`.meme4`)
+const meme5 = document.querySelector(`.meme5`)
+const meme6 = document.querySelector(`.meme6`)
+const meme7 = document.querySelector(`.meme7`)
+const meme8 = document.querySelector(`.meme8`)
+
+meme1.addEventListener(`click`, () => {
+    const sound1 = new Audio("audio/Why are You Running.mp3")
+    sound1.play()
+})
+
+meme2.addEventListener(`click`, () => {
+    const sound2 = new Audio("audio/Noice. - Sound effect..mp3")
+    sound2.play()
+})
+
+meme3.addEventListener(`click`, () => {
+    const sound3 = new Audio("audio/No Copyright Oh My God Wow Sound Effect.mp3")
+    sound3.play()
+})
+
+meme4.addEventListener(`click`, () => {
+    const sound4 = new Audio("audio/OH BABY A TRIPLE - MLG Sound Effects (HD).mp3")
+    sound4.play()
+})
+
+meme5.addEventListener(`click`, () => {
+    const sound5 = new Audio("audio/Oh Hell No VINE.mp3")
+    sound5.play()
+})
+
+meme6.addEventListener(`click`, () => {
+    const sound6 = new Audio("audio/Damn Son Where'd You Find This - Sound Effect (HD).mp3")
+    sound6.play()
+})
+
+meme7.addEventListener(`click`, () => {
+    const sound7 = new Audio("audio/He need some milk sound effect.mp3")
+    sound7.play()
+})
+
+meme8.addEventListener(`click`, () => {
+    const sound8 = new Audio("audio/Why you heff to be mad (Original).mp3")
+    sound8.play()
+})
